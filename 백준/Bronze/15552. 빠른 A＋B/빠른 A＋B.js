0d0acf48763f760de6299070bb575c05da50a3da
leear613 <leear613@daum.net>
2025-03-21 13:18:20 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1344 ms, Memory: 180044 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const T = Number(input[0]);
+let result = '';
+
+for (let i = 1; i <= T; i++) {
+    const [A, B] = input[i].split(' ').map(Number);
+    result += (A + B) + '\n';
+}
+
+console.log(result);
