9d25a5c1d6562f5c293aecf9ede084166a96611f
leear613 <leear613@daum.net>
2025-03-31 12:48:55 +0900

    [Bronze III] Title: 최소, 최대, Time: 376 ms, Memory: 93316 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const numbers = input[1].split(' ').map(Number);
+
+const min = Math.min(...numbers);
+const max = Math.max(...numbers);
+
+console.log(min, max);
